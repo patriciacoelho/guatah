7c011351bafc367b65d0d95618d11bdcc048643b
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2022-12-23 16:23:08 -0300

    feat: estiliza componente de ListItem

--- lib/widgets/list_item.dart
+++ lib/widgets/list_item.dart
@@ -1,42 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:guatah/constants/colors.dart';
+import 'package:guatah/screens/details/details.dart';
+import 'package:ionicons/ionicons.dart';
 
 class ListItem extends StatelessWidget {
+  final String id;
   final String title;
   final String? imageUrl;
   final String? secondaryInfo;
   final String? extraInfo;
-  const ListItem({ super.key, required this.title, this.imageUrl, this.secondaryInfo, this.extraInfo });
+  const ListItem({ super.key, required this.title, this.imageUrl, this.secondaryInfo = '', this.extraInfo = '', required this.id });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(bottom: 8),
-      child: Row(
-        children: [
-          imageUrl != null ?
-            Image.network(
-              imageUrl!,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => DetailsPage(id: id)),
+          );
+        },
+        child: Row(
+          children: [
+            Container(
               width: 90,
               height: 90,
-            ) : Container(
-                width: 90,
-                height: 90,
-                decoration: BoxDecoration(
-                  border: Border.all(color: const Color(0xFF777777)),
-                  borderRadius: BorderRadius.circular(16),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xFFF5F5F5)),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: imageUrl != null ? ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.network(
+                  imageUrl!,
+                  width: 90,
+                  height: 90,
+                  fit: BoxFit.fill,
                 ),
+              ) : null,
             ),
-          Container(
-            padding: const EdgeInsets.only(left: 12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(title),
-                Text(extraInfo!),
-              ],
+            Container(
+              padding: const EdgeInsets.only(left: 12, bottom: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      color: primaryColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    secondaryInfo!,
+                    style: const TextStyle(
+                      color: mediumGreyColor,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Ionicons.calendar_outline,
+                        color: mediumGreyColor,
+                        size: 14.0,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 5, left: 4),
+                        child: Text(
+                          extraInfo!,
+                          style: const TextStyle(
+                            color: mediumGreyColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
