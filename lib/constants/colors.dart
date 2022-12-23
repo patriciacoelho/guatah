b40c4a146ec5b4d9c159708dc6c32b8463fcd082
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2022-12-23 16:24:06 -0300

    feat: adiciona cor em arquivo de constantes

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xFFF25E5E);
 const Color backgroundColor = Color(0xFFFFFFFF);
+const Color mediumGreyColor = Color(0xFF777777);
