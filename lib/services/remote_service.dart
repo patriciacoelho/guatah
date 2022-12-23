e61166f7ed932774803798cd5fa781927c891bc4
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2022-12-23 12:40:36 -0300

    refactor: utiliza url da api da variável de ambiente

--- lib/services/remote_service.dart
+++ lib/services/remote_service.dart
@@ -8,11 +8,16 @@ class RemoteService {
   Future<List<Itinerary>?> getItineraries() async {
     var client = http.Client();
 
-    var uri = Uri.parse('https://fb48-2804-5fb8-c005-7200-e095-3702-58d-6ecd.sa.ngrok.io/itineraries');
+    const apiBaseUrl = String.fromEnvironment('API_BASE_URL');
+    log("debug message", error: apiBaseUrl);
+    var uri = Uri.parse('$apiBaseUrl/itineraries');
+    log("debug message", error: uri);
     var response = await client.get(uri);
     if (response.statusCode == 200) {
       var data = response.body;
       return itineraryFromJson(data);
     }
   }
+
+  // Future<List<City>?> getCities() async {}
 }
